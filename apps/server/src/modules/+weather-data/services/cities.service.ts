cc144cfe26764aa1875d6ecdb5e9be65c3995a01
NikitaGlukhi <nikita.glukhi@valor-software.com>
2020-02-21 10:10:14 +0200

    feat(core): added cities service and cities model

--- apps/server/src/modules/+weather-data/services/cities.service.ts
+++ apps/server/src/modules/+weather-data/services/cities.service.ts
@@ -3,12 +3,29 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { CityEntity } from '../entities/city.entity';
+import { CountryEntity } from '../entities/country.entity';
 
 @Injectable()
 export class CitiesService {
 
   constructor(
-    @InjectRepository(CityEntity) private readonly cityEntity: Repository<CityEntity>
+    @InjectRepository(CityEntity) private readonly cityEntity: Repository<CityEntity>,
+    @InjectRepository(CountryEntity) private readonly countryEntity: Repository<CountryEntity>
   ) {}
+
+  async getCity(cityName: string): Promise<void | CityEntity> {
+    return this.cityEntity.findOne({
+      where: { name: cityName }
+    })
+      .then(res => {
+        if (!res) {
+
+        }
+        return res;
+      })
+      .catch(err => {})
+  }
+
+  private addNewcity(cityName: string) {}
 
 }
